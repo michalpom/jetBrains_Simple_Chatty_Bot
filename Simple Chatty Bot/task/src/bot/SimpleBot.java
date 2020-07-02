09450cdc6b72040f9a62c4299888100b708d6076
michalpom <michalpom@gmail.com>
2020-07-02 20:51:36 +0200

    The project is finished.

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -6,11 +6,11 @@ public class SimpleBot {
     final static Scanner scanner = new Scanner(System.in); // Do not change this line
 
     public static void main(String[] args) {
-        greet("Aid", "2018"); // change it as you need
+        greet("Bot", "2020"); // change it as you need
         remindName();
         guessAge();
         count();
-        // ...
+        test();
         end();
     }
 
@@ -45,7 +45,19 @@ public class SimpleBot {
 
     static void test() {
         System.out.println("Let's test your programming knowledge.");
-        // write your code here
+        System.out.println("Why do we use methods?\n" +
+                "1. To repeat a statement multiple times.\n" +
+                "2. To decompose a program into several small subroutines.\n" +
+                "3. To determine the execution time of a program.\n" +
+                "4. To interrupt the execution of a program.");
+        int answer = scanner.nextInt();
+
+        while(answer != 2){
+            System.out.println("Please, try again.");
+            answer = scanner.nextInt();
+        }
+
+        System.out.println("Congratulations, have a nice day!");
     }
 
     static void end() {
